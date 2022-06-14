5f0187ecf0453b564b4dc88ad5c9d32734db359d
Bryant shi <bryanthermawan@gmail.com>
2022-06-14 10:54:17 +0800

    Push notif works !!

--- Afoirm/App.js
+++ Afoirm/App.js
@@ -1,12 +1,33 @@
+import axios from 'axios';
 import registerNNPushToken from 'native-notify';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+
+
+function buttonpress() {
+  console.log("Ahhhhh it hurts")
+  console.log(`Before you pressed me, count was ${count}`)
+  setCount(count+1)
+}
 
 export default function App() {
+  registerNNPushToken(2960, 'AkBsj1bVoC7DXXJuKkmF4Z');
+
+  let [count, setCount] = useState(0)
+
+  function buttonpress() {
+    console.log("Ahhhhh it hurts")
+    console.log(`Before you pressed me, count was ${count}`)
+    setCount(count+1)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Click the button below!</Text>
+        <Button title="Press me!!" onPress={buttonpress}></Button>
+      <Text>You have press it {count} times </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,4 +40,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  Button: {
+    width: 100,
+    height: 50,
+  }
 });
